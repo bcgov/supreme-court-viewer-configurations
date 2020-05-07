5fe5070a6bf2b7489df2a6f35163ab8dfbe2be50
Wade Barnes <wade.barnes@shaw.ca>
2020-05-07 09:11:22 -0700

    Integrate request settings.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/templates/api/api-deploy.overrides.sh
+++ openshift/templates/api/api-deploy.overrides.sh
@@ -21,11 +21,15 @@ if createOperation; then
 
   # Get the settings for file services client ...
   readParameter "FILE_SERVICES_CLIENT_URL - Please provide the URL to the File Services API.  The default is a blank string." FILE_SERVICES_CLIENT_URL "" "false"
-  readParameter "FILE_SERVICES_CLIENT_USERNAME - Please the username for use with the File Services API.  The default is a randomly gererated username." FILE_SERVICES_CLIENT_USERNAME $(generateUsername) "false"
-  readParameter "FILE_SERVICES_CLIENT_PASSWORD - Please the password for use with the File Services API.  The default is a randomly gererated password." FILE_SERVICES_CLIENT_PASSWORD $(generatePassword) "false"
+  readParameter "FILE_SERVICES_CLIENT_USERNAME - Please provide the username for use with the File Services API.  The default is a randomly gererated username." FILE_SERVICES_CLIENT_USERNAME $(generateUsername) "false"
+  readParameter "FILE_SERVICES_CLIENT_PASSWORD - Please provide the password for use with the File Services API.  The default is a randomly gererated password." FILE_SERVICES_CLIENT_PASSWORD $(generatePassword) "false"
+  readParameter "REQUEST_APPLICATION_CODE - Please provide the Request Application Code for use with the File Services API.  The default is 'not-set'." REQUEST_APPLICATION_CODE "not-set" "false"
+  readParameter "REQUEST_AGENCY_IDENTIFIER_ID - Please provide the Request Agency Identifier Id for use with the File Services API.  The default is a randomly gererated value." REQUEST_AGENCY_IDENTIFIER_ID $(generatePassword) "false"
+  readParameter "REQUEST_PART_ID - Please provide the Request Part Id for use with the File Services API.  The default is a randomly gererated value." REQUEST_PART_ID $(generatePassword) "false"
 else
   # Secrets are removed from the configurations during update operations ...
-  printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials.\nSkipping the prompts for FILE_SERVICES_CLIENT_URL, FILE_SERVICES_CLIENT_USERNAME, and FILE_SERVICES_CLIENT_PASSWORD secrets ...\n"
+  writeParameter "REQUEST_PART_ID" "prompt_skipped" "false"
+  printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials.\nSkipping the prompts for FILE_SERVICES_CLIENT_URL, FILE_SERVICES_CLIENT_USERNAME, FILE_SERVICES_CLIENT_PASSWORD, REQUEST_APPLICATION_CODE, REQUEST_AGENCY_IDENTIFIER_ID, and secrets ...\n"
   writeParameter "USER_ID" "generation_skipped" "false"
   writeParameter "USER_PASSWORD" "generation_skipped" "false"
   writeParameter "ADMIN_USER_ID" "generation_skipped" "false"
@@ -34,6 +38,9 @@ else
   writeParameter "FILE_SERVICES_CLIENT_URL" "prompt_skipped" "false"
   writeParameter "FILE_SERVICES_CLIENT_USERNAME" "prompt_skipped" "false"
   writeParameter "FILE_SERVICES_CLIENT_PASSWORD" "prompt_skipped" "false"
+  writeParameter "REQUEST_APPLICATION_CODE" "prompt_skipped" "false"
+  writeParameter "REQUEST_AGENCY_IDENTIFIER_ID" "prompt_skipped" "false"
+  writeParameter "REQUEST_PART_ID" "prompt_skipped" "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
